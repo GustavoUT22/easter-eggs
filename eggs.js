f3344602a75444458a876c51c7b44c9e48c5c8b2
Gustavout22 <gustavougartetorres@gmail.com>
2023-05-19 15:53:39 -0500

    add: marquee

--- eggs.js
+++ eggs.js
@@ -26,3 +26,43 @@ function Loader(frames = loaderClocks) {
   }
 
   //Marquee
+  function Marquee(sentence, spaces) {
+    this.frames = [];
+    pos = 0
+
+    this.fill = () => { for (let i = spaces; i >= 0; i--) {
+     let a = " ".repeat(i) + sentence
+     this.frames.push(a)
+    };
+    }
+
+    this.erase = () => { 
+        let size = sentence.length
+        let arr = sentence.split("")
+
+        for (let i = 0; i < size; i++) {
+          arr.shift()
+          this.frames.push(arr.join(""))
+        }
+    }
+
+  this.render = function () {
+    return setInterval(() => {
+    if (pos >= this.frames.length) pos = 0;
+    console.clear();
+    console.log(this.frames[pos]);
+    pos += 1;}, 500)
+  };
+
+  this.run = () => {
+    this.fill()
+    this.erase()
+    const intervalId = this.render();
+
+    setTimeout(() => {
+      clearInterval(intervalId);
+      console.clear();
+    }, (spaces + sentence.length + 1) * 500);
+  };
+
+}
